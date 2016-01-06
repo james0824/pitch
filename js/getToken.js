21b723625e675116e445e75a48f3c4dc6d37fd67
James <809825634@qq.com>
2016-01-06 20:23:47 +0800

    James

--- js/getToken.js
+++ js/getToken.js
@@ -1,14 +1,19 @@
 // JavaScript Document
 function getToken(){
 	var a = document.cookie.split("=");
-	var reg = new RegExp(a[0]);
-	if (reg.test("token")){
-		return a[1];
-	}
-	else {
-		alert("出错了，请重新登陆");
-		window.location.href="http://222.201.132.27/crewManageEvolve";
+	var k = 1;
+	var i = 0;
+	var t;
+	while (k){
+		var reg = new RegExp(a[i]);
+		if (reg.test("token")){
+			t = a[i+1];
+			k = 0;
+		}
+		if (a[i]==""){
+			alert("出错了，请重新登陆");
+			window.location.href="http://222.201.132.27/crewManageEvolve/#/login";
+		}
 	}
 	//获取本地cookie中的token
-
 }
